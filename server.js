51d37118730c2327a8c44f34f50285353c085361
Mikeysoar <mwclimb@gmail.com>
2021-07-29 19:06:36 -0600

    20210729 1906

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ const mainMenu = async () => {
         type: 'list',
         choices: [
             'View All Employees',
+            'View All Roles',
             'View All Employees By Department',
             'View Employees by Manager',
             'Add Employee',
-            'Remove Employee',
+            'Add Department',
+            "Add a Role",
             'Add Employee Role',
             'Update Employee Role',
             'View All Departments',
             'Update Employee Manager',
             'Exit'
-            //    add more choices
         ]
     }]);
     console.log(mainQuestion);
@@ -25,7 +26,10 @@ const mainMenu = async () => {
         const allEmployees = await db.query('SELECT * FROM employee')
         console.table(allEmployees);
         mainMenu();
-        // (bonus)
+    } else if (mainQuestion.answer === 'View All Roles') {
+        const allRoles = await db.query('SELECT * from role')
+        console.table(allRoles);
+        mainMenu();
     } else if (mainQuestion.answer === 'View All Employees By Department') {
         const employeeByDepartment = await db.query('SELECT * FROM employee LEFT JOIN role ON employee.role_id = role.id')
         console.table(employeeByDepartment);
@@ -35,11 +39,10 @@ const mainMenu = async () => {
         const employeeByManager = await db.query('SELECT concat(manager.first_name, " ", manager.last_name) AS manager, employee.first_name AS employee FROM employee LEFT JOIN employee manager ON manager.id = employee.manager_id WHERE employee.manager_id IS NOT NULL; ')
         console.table(employeeByManager);
         mainMenu();
-        //  this one is broken
     } else if (mainQuestion.answer === 'Add Employee') {
-        const roleDb = await db.query( 'SELECT title, id FROM role')
+        const roleDb = await db.query('SELECT title, id FROM role')
         const managerDb = await db.query('SELECT first_name, last_name, id FROM employee')
-    
+
         const addEmployeeInput = await inquirer.prompt([
             {
                 type: 'input',
@@ -55,51 +58,79 @@ const mainMenu = async () => {
                 type: 'list',
                 name: 'role_id',
                 message: 'What is the employee role?',
-                choices: roleDb.map(role => ({name: role.title, value: role.id})) 
+                choices: roleDb.map(role => ({ name: role.title, value: role.id }))
             },
             {
                 type: 'list',
                 name: 'manager_id',
                 message: 'Who will this employee report too?',
-                choices: managerDb.map(manager => ({name: manager.first_name + manager.last_name, value: manager.id }))                                
+                choices: managerDb.map(manager => ({ name: manager.first_name + manager.last_name, value: manager.id }))
             },
         ])
         const addEmployee = await db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,
-         [
-            addEmployeeInput.first_name,
-            addEmployeeInput.last_name,
-            addEmployeeInput.role_id,
-            addEmployeeInput.manager_id
-        ]);
-        
+            [
+                addEmployeeInput.first_name,
+                addEmployeeInput.last_name,
+                addEmployeeInput.role_id,
+                addEmployeeInput.manager_id
+            ]);
+
         console.table(addEmployee);
         mainMenu();
-        // this one is broken (bonus)
-    } else if (mainQuestion.answer === 'Remove Employee') {
-        const removeEmployee = await db.query('DELETE from employee WHERE id = ?');
-        console.log(removeEmployee);
+        // this one is broken
+    } else if (mainQuestion.answer === 'Add Department') {
+        const addDepartment = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What department would you like to add?'
+            },
+        ])
+        const departmentInput = await db.query('INSERT INTO department (name) VALUES (?)',);
+        console.table(addDepartment);
         mainMenu();
+
         // this one is broken
     } else if (mainQuestion.answer === 'Add Employee Role') {
-        const addRole = await db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)' );
-        console.table (addRole);
+        const addRoleInput = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the job title?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary?'
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'What is the department ID?'
+            },
+        ])
+        const addRole = await db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)',
+            [
+                addRoleInput.title,
+                addRoleInput.salary,
+                addRoleInput.department_id
+            ]
+        );
+        console.table(addRole);
         mainMenu();
+        // this one is still broken
     } else if (mainQuestion.answer === 'Update Employee Role') {
         const updateEmployeeRole = await db.query('UPDATE employee')
         // need to update employee role
         console.log(updateEmployeeRole);
         mainMenu();
+        // this one works
     } else if (mainQuestion.answer === 'View All Departments') {
         const allDepartments = await db.query('SELECT * FROM department')
         console.table(allDepartments);
         mainMenu();
-        // this one is broken (bonus)
-    } else if (mainQuestion.answer === 'Update Employee Manager') {
-        const updateEmployeeManager = await db.query('UPDATE employee = ? WHERE manager = ?')
-        console.table(updateEmployeeManager);
-        mainMenu(); 
-    }else if (mainQuestion.answer === 'Exit') {
-        process.exit();        
+    } else if (mainQuestion.answer === 'Exit') {
+        process.exit();
     }
 
 }
